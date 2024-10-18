1325c9c27ae468974febf0de10cfe49eb4027e04
repo <khvits21@gmail.com>
2024-10-19 01:04:07 +0300

    Add MainContent component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css"
 import Sidebar from "./components/Sidebar"
+import MainContent from './components/MainContent';
 
 function App() {
 
     const projects = ['Work 👜', 'Groceries 🛒', 'Reading List 📚', 'Personal 📝'];
+    const tasks = [
+        { id: 1, text: 'work 1', completed: false },
+        { id: 2, text: 'work 2', completed: false },
+        { id: 3, text: 'work 3', completed: false },
+    ];
 
     return(
         <div className="app">
             <Sidebar projects={projects} />
-            <h1>My todoList-app</h1>
+            <MainContent tasks={tasks} />
         </div>
     )
 }
