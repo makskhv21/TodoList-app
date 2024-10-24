7d10cd2fbc2842ef7e1fab87f4e795cf8ea73c08
repo <khvits21@gmail.com>
2024-10-24 15:58:26 +0300

    Update Sidebar to edit projects inline and set icon

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,11 +1,9 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faSave, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-
-function Sidebar( { projects, setSelectedProject, addProject, editProject, deleteProject } ) {
-    
+function Sidebar({ projects, setSelectedProject, addProject, editProject, deleteProject }) {
     const [newProject, setNewProject] = useState('');
-    const [editMode, setEditMode] = useState(false);
-    const [editedProject, setEditedProject] = useState(null);
     const [editedProjectName, setEditedProjectName] = useState('');
 
     const handleAddProject = () => {
@@ -14,54 +12,63 @@ function Sidebar( { projects, setSelectedProject, addProject, editProject, delet
     };
 
     const handleEditProject = (project) => {
-        setEditMode(true);
-        setEditedProject(project);
         setEditedProjectName(project);
     };
 
-    const handleSaveEdit = () => {
-        editProject(editedProject, editedProjectName);
-        setEditMode(false);
-        setEditedProject(null);
+    const handleSaveEdit = (project) => {
+        editProject(project, editedProjectName);
         setEditedProjectName('');
     };
 
-
-    return(
-            <div className="sidebar">
-                <div className="sidebar-item">📋 Inbox</div>
-                <div className="sidebar-item">📅 Today</div>
-                <div className="sidebar-item">🗓️ Next 7 days</div>
-                <hr />
-                <div className="projects">
-                    {projects.map((project, index) => (
+    return (
+        <div className="sidebar">
+            <div className="sidebar-item">📋 Inbox</div>
+            <div className="sidebar-item">📅 Today</div>
+            <div className="sidebar-item">🗓️ Next 7 days</div>
+            <hr />
+            <div className="projects">
+                {projects.map((project, index) => (
                     <div key={index} className="sidebar-item">
-                        <span onClick={() => setSelectedProject(project)}>{project}</span>
-                        <button onClick={() => handleEditProject(project)}>Edit</button>
-                        <button onClick={() => deleteProject(project)}>Delete</button>
-                    </div>
-                    ))}
-                    {editMode && (
-                        <div className="sidebar-item">
-                            <input 
-                                value={editedProjectName} 
-                                onChange={(e) => setEditedProjectName(e.target.value)} 
-                            />
-                            <button onClick={handleSaveEdit}>Save</button>
+                        <span onClick={() => setSelectedProject(project)}>
+                            {project === editedProjectName ? (
+                                <input
+                                    value={editedProjectName}
+                                    onChange={(e) => setEditedProjectName(e.target.value)}
+                                    onBlur={() => handleSaveEdit(project)}
+                                    onKeyDown={(e) => e.key === "Enter" && handleSaveEdit(project)}
+                                />
+                            ) : (
+                                project
+                            )}
+                        </span>
+                        <div className='btn-save-edit-delete'>
+                            {project === editedProjectName ? (
+                                <button onClick={() => handleSaveEdit(project)}>
+                                    <FontAwesomeIcon icon={faSave} />
+                                </button>
+                            ) : (
+                                <button onClick={() => handleEditProject(project)}>
+                                    <FontAwesomeIcon icon={faEdit} /> 
+                                </button>
+                            )}
+                            <button onClick={() => deleteProject(project)}>
+                                <FontAwesomeIcon icon={faTrash} />
+                            </button>
                         </div>
-                    )}
-                    <div className="sidebar-item">
-                        <input 
-                            type="text" 
-                            value={newProject} 
-                            onChange={(e) => setNewProject(e.target.value)} 
-                            placeholder="New project name"
-                        />
-                        <button onClick={handleAddProject}>+ Add Project</button>
                     </div>
+                ))}
+                <div className="sidebar-item">
+                    <input
+                        type="text"
+                        value={newProject}
+                        onChange={(e) => setNewProject(e.target.value)}
+                        placeholder="New project name"
+                    />
+                    <button onClick={handleAddProject}>+ Add Project</button>
                 </div>
             </div>
-    )
+        </div>
+    );
 }
 
 export default Sidebar;
