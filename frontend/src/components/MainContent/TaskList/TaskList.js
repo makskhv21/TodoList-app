00675aac46992d332d1a4de772b14ff9e90685c1
repo <khvits21@gmail.com>
2024-11-01 15:34:34 +0200

    fix: Enable task menu for completed tasks

--- frontend/src/components/MainContent/TaskList/TaskList.js
+++ frontend/src/components/MainContent/TaskList/TaskList.js
@@ -172,6 +172,7 @@ function TaskList({
                             editingTaskText={editingTaskText}
                             setEditingTaskText={setEditingTaskText}
                             handleSaveEdit={handleSaveEdit}
+                            onClick={() => handleTaskClick(task)}
                         />
                     ))}
                 </div>
